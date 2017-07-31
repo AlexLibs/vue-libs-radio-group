31e4241c76c0d90105849fce06a1a2d567fb19f6
Libster <al@memeglobal.com>
2017-07-31 20:07:47 +0300

    vue-libs-radio-group component

---
+++ README.md
@@ -0,0 +1,54 @@
+# Vue Simple Toggle (Switch)
+
+A simple and super light Vue.js component for fancy radio group (state switch).
+
+No dependencies. No sass/less compilation. Very easy to use :).
+
+<img src="https://raw.githubusercontent.com/AlexLibs/vue-libs-simple-toggle/master/demo/vue-libs-simple-toggle-demo.png" /><br>
+
+## Installation
+
+```bash
+npm install vue-libs-radio-group --save
+```
+
+## Basic Usage
+
+```javascript
+import GroupButton from 'vue-libs-radio-group';
+
+new Vue({
+
+    components: {
+        GroupButton
+    },
+
+    data () {
+        return {
+            currentState: 'all'
+        }
+    }
+};
+```
+
+```html
+
+<group-button v-bind:options="{active: 'Show Active', inactive: 'Show Inactive', all: 'Show All'}" v-model="currentState"></group-button>
+
+```
+
+## Customization
+
+Override the relevant classes to customize it. For example, to make it all blue instead of #dc0606:
+
+```css
+    .vue-libs-radio-group {
+        border: 1px solid blue;
+    }
+    .vue-libs-radio-group label {
+        border-right: 1px solid blue;
+    }
+    .vue-libs-radio-group input[type="radio"]:checked + label {
+        background-color: blue;
+    }
+```
---
+++ vue_libs_radio_group.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="vue-libs-radio-group">
+        <template v-for="(label, id) in options">
+            <input type="radio" v-bind:value="id" v-model="model" v-bind:id="id">
+            <label class="btn btn-primary" v-bind:for="id">
+                {{label}}
+            </label>
+        </template>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                model: this.value,
+            };
+        },
+        props: [
+            'options',
+            'value',
+        ],
+        methods: {
+        },
+        components: {},
+        watch: {
+            model: function(){
+                this.$emit('input', this.model)
+            }
+        }
+    }
+</script>
+
+<style>
+    .vue-libs-radio-group {
+        padding: 8px 0;
+        border: 1px solid #dc0606;
+        border-radius: 30px;
+        display: inline-block;
+    }
+    .vue-libs-radio-group input[type="radio"] {
+        display: none;
+    }
+    .vue-libs-radio-group label {
+        border-right: 1px solid #dc0606;
+        cursor: pointer;
+        padding: 9px;
+    }
+    .vue-libs-radio-group label:last-child {
+        border-right: none;
+        margin-right: -4px;
+        border-radius: 0 30px 30px 0;
+    }
+    .vue-libs-radio-group label:first-of-type {
+        border-radius: 30px 0 0 30px;
+    }
+    .vue-libs-radio-group input[type="radio"]:checked + label {
+        color: white;
+        background-color: #dc0606;
+    }
+</style>
